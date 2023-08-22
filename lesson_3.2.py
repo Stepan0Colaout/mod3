ba0a422184748612969771455fac2df5381c70e4
Степан <gerev560@gmail.com>
2023-08-22 23:06:56 +0300

    Первый комит

---
+++ lesson_3.2.py
@@ -0,0 +1,32 @@
+a = 'ABCAAC1C'                      #N=8    (кол-во символов)
+
+def strcounter(a):                  #O(N**2)
+    for char in a:
+        counter = 0
+        for sub_char in a:
+            if char == sub_char:
+                counter += 1
+        print(char, counter)
+
+#strcounter(a)
+
+
+def strcounter1(a):                  #O(N*M)    M=4(кол-во уникальных символов)
+    for char in set(a):
+        counter = 0
+        for sub_char in a:
+            if char == sub_char:
+                counter += 1
+        print(char, counter)
+
+
+
+def strcounter2(a):                 #O(N)
+    syms_counter = {}
+    for char in a:
+        syms_counter[char] = syms_counter.get(char, 0) + 1
+    print(syms_counter)
+
+strcounter2(a)
+
+print("Hello")
